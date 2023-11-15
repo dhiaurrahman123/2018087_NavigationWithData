7d25384ed2e72b79f6f937a5e87c1c8684d6d9a8
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-11-15 19:39:33 +0700

    menambahkan viewmodel baru

--- app/src/main/java/com/example/activity7/ui/theme/OrderViewModel.kt
+++ app/src/main/java/com/example/activity7/ui/theme/OrderViewModel.kt
@@ -3,10 +3,31 @@ package com.example.activity7.ui.theme
 import android.icu.text.NumberFormat
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.example.activity7.Data2.ContactUiState
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import androidx.compose.runtime.*
+
 
 private const val HARGA_PER_CUP = 3000
 
 class OrderViewModel : ViewModel() {
+    private val _stateUI = MutableStateFlow(ContactUiState())
+    val stateUi: StateFlow<ContactUiState> = _stateUI.asStateFlow()
+
+    fun setContact(List: MutableList<String>)
+    {
+        _stateUI.update {
+                stateSaatini->
+            val list
+            stateSaatini.copy(
+                nama = list[0],
+                alamat = list[1],
+                tlpn = list[2]
+            )
+        }
+    }
     private val _stateUI = MutableStateFlow(OrderUIState())
     val statedUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
 
